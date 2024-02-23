934bd78656ba0af807da9e437999d42f7211bb50
Aayush-G18 <goyalaayush292@gmail.com>
2024-02-23 20:11:19 +0530

    can add a book through modal

--- app.js
+++ app.js
@@ -1,37 +1,70 @@
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author,read) {
     this.title = title;
     this.author = author;
-    this.info = function () {
-        return`${this.title} by ${this.author}`;
-    };
+
 }
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}`;
+};
 
 function addBookToLibrary(title, author) {
     const newBook = new Book(title, author);
     myLibrary.push(newBook);
+    renderLibrary();
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
-
-
-document.addEventListener("DOMContentLoaded", function () {
+function renderLibrary() {
     var container = document.querySelector('.container');
+    container.innerHTML = '';
 
-    // Add books to the library
-    addBookToLibrary("Harry Potter", "J.K. Rowling");
-    addBookToLibrary("To Kill a Mockingbird", "Harper Lee");
-
-    // Loop through the library and create book elements
-    myLibrary.forEach(function (book) {
+    myLibrary.forEach(function(book) {
         var child = document.createElement('div');
-        // child.textContent=`${book.title} by ${book.author}`;
-        child.textContent=book.info();
+        child.textContent = book.info();
         child.classList.add('child');
         container.appendChild(child);
     });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var container = document.querySelector('.container');
+    
+    addBookToLibrary("Harry Potter", "J.K. Rowling");
+    addBookToLibrary("To Kill a Mockingbird", "Harper Lee");
+    addBookToLibrary("The Hobbit", "J.R.R. Tolkien");    
+    renderLibrary();
+});
+
+const showBtn = document.getElementById("show-dialog");
+const dialog = document.getElementById("dialog");
+const title = document.getElementById("title");
+const confirmBtn = document.getElementById("confirmBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+
+showBtn.addEventListener("click", () => {
+    dialog.style.top = "-400px";
+    dialog.style.left = "-400px";
+    dialog.showModal();
+});
+
+dialog.addEventListener("close", (e) => {
+    output.value =
+        dialog.returnValue === "default"
+        ? "No return value."
+        : `ReturnValue: ${dialog.returnValue}.`; 
+});
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    const confirmValue = new Book(title.value,author.value);
+    addBookToLibrary(confirmValue.title,confirmValue.author);
+    dialog.close(); 
+});
+
+cancelBtn.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    dialog.close();
 });
