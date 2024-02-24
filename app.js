e1a288f9284de152f910bc01ec39a6e73ab12aaf
Aayush-G18 <goyalaayush292@gmail.com>
2024-02-24 14:28:37 +0530

    Delete functionality added

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const myLibrary = [];
 function Book(title, author,read) {
     this.title = title;
     this.author = author;
-
 }
 
 Book.prototype.info = function() {
@@ -16,17 +15,43 @@ function addBookToLibrary(title, author) {
     renderLibrary();
 }
 
+function deleteBookFromLibrary(index) {
+    myLibrary.splice(index,1);
+    renderLibrary();
+}
+
 function renderLibrary() {
     var container = document.querySelector('.container');
     container.innerHTML = '';
 
-    myLibrary.forEach(function(book) {
+    myLibrary.forEach(function(book, index) {
         var child = document.createElement('div');
         child.textContent = book.info();
+        var deleteBtn = document.createElement('button');
+        var image = document.createElement("img");
+        image.src = "dustbin.png";
+        image.style.width = "30px";
+        deleteBtn.style.border = "none";
+        deleteBtn.classList.add('deleteBtn');
+        deleteBtn.appendChild(image);
+        deleteBtn.setAttribute('data-book-index', index); 
+        child.appendChild(deleteBtn);
         child.classList.add('child');
         container.appendChild(child);
     });
 }
+
+
+
+document.querySelector('.container').addEventListener('click', function(event) {
+    if (event.target && (event.target.matches('.deleteBtn') || event.target.matches('.deleteBtn img'))) {
+        const deleteBtn = event.target.closest('.deleteBtn');
+        const bookIndex = deleteBtn.dataset.bookIndex;
+        deleteBookFromLibrary(bookIndex);
+    }
+});
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -59,8 +84,9 @@ dialog.addEventListener("close", (e) => {
 
 confirmBtn.addEventListener("click", (event) => {
     event.preventDefault();
+    //default form validation working so js validation
     if (title.value.trim() === '' || author.value.trim() === '') {
-        alert('Please fill in both title and author fields.');
+        alert('Please fill in both title and author fields.'); n 
         event.preventDefault();
     }
     else{
